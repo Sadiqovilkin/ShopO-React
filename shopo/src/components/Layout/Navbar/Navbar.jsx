c17bb6e6305894868e481414b5ed7316a7550e88
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-01-30 19:11:28 +0400

    #Finished navbar&Banner

--- shopo/src/components/Layout/Navbar/Navbar.jsx
+++ shopo/src/components/Layout/Navbar/Navbar.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
-
+import React from "react";
+import "./Navbar.css";
+import { SlRefresh } from "react-icons/sl";
+import { BsBasket3, BsChevronRight } from "react-icons/bs";
+import { BsPerson, BsHeart } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav id="nav1">
+      <div className="upside">
+        <div className="container pt-3">
+          <div className="row align-items-center">
+            <div className="col-lg-3">
+              <div className="logo">
+                <img
+                  src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="forms">
+                <form action="">
+                  <input type="text" placeholder="Search Product ..." />
+                  <select id="product_categories">
+                    <option value="volvo">All Categories</option>
+                  </select>
+                  <button id="btn_search">Search</button>
+                </form>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <div className="icons">
+                <div className="item1">
+                  <SlRefresh style={{ fontSize: "22px" }} />
+                  <div className="box">2</div>
+                </div>
+                <div className="item1">
+                  <BsHeart style={{ fontSize: "22px" }} />
+                  <div className="box">1</div>
+                </div>
+                <div className="item1">
+                  <BsBasket3 style={{ fontSize: "22px" }} />
+                  <div className="box">1</div>
+                </div>
 
-export default Navbar
+                <div className="item1">
+                  <BsPerson style={{ fontSize: "22px" }} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="downside">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
+              <select id="product_categories">
+                <option value="volvo">All Categories</option>
+                <option value="volvo">Mobile&Phone</option>
+                <option value="volvo">Computers</option>
+              </select>
+            </div>
+            <div className="col-lg-6 nav_items">
+              <ul>
+                <li>
+                  Homepage <BiChevronDown />
+                </li>
+                <li>
+                  Shop <BiChevronDown />
+                </li>
+                <li>
+                  Pages <BiChevronDown />
+                </li>
+                <li>About</li>
+                <li>Blog </li>
+                <li>Contact </li>
+              </ul>
+            </div>
+            <div className="col-lg-3 become">
+              <a href="#">
+                Become a Seller <BsChevronRight />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- shopo/src/components/Pages/Home/Banner/Banner.jsx
+++ shopo/src/components/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import { useEffect } from "react";
+
+import BannerStyled from "./Banner.module.css";
+import Samsung from "../../../../assets/banner-1.png";
+import SmartWatch from "../../../../assets/banner-2.png";
+import Xbox from "../../../../assets/banner-3.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { AiOutlineShoppingCart, AiOutlineSecurityScan } from "react-icons/ai";
+import { BiRefresh } from "react-icons/bi";
+import { TfiCup } from "react-icons/tfi";
 
 const Banner = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div>Banner</div>
-  )
-}
+    <section className={BannerStyled.banner}>
+      <div className="container px-3">
+        <div className="row px-5">
+          <div className="col-xl-8" data-aos="fade-right">
+            <div className={BannerStyled.samsung}>
+              <img src={Samsung} alt="" />
+            </div>
+          </div>
+          <div className="col-xl-4" data-aos="fade-left">
+            <div className="col-xl-12 px-0">
+              <div className={BannerStyled.smart}>
+                <img src={SmartWatch} alt="" />
+              </div>
+            </div>
+            <div className="col-xl-12 px-0">
+              <div className={BannerStyled.xbox}>
+                <img src={Xbox} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-12" data-aos="fade-up">
+            <div className={BannerStyled.white_col_adapt}>
+              <div className={BannerStyled.mini_cart}>
+                <div className={BannerStyled.mini_cart_icon}>
+                  <AiOutlineShoppingCart />
+                </div>
+                <div className={BannerStyled.mini_cart_text}>
+                  <h6>Free Shipping</h6>
+                  <p>When ordering over $100</p>
+                </div>
+              </div>
+              <div className={BannerStyled.mini_cart}>
+                <div className={BannerStyled.mini_cart_icon}>
+                  <BiRefresh />
+                </div>
+                <div className={BannerStyled.mini_cart_text}>
+                  <h6>Free Shipping</h6>
+                  <p>Get Return within 30 days</p>
+                </div>
+              </div>
+              <div className={BannerStyled.mini_cart}>
+                <div className={BannerStyled.mini_cart_icon}>
+                  <AiOutlineSecurityScan />
+                </div>
+                <div className={BannerStyled.mini_cart_text}>
+                  <h6>Free Shipping</h6>
+                  <p>100% Secure Online Payment</p>
+                </div>
+              </div>
+              <div className={BannerStyled.mini_cart}>
+                <div className={BannerStyled.mini_cart_icon}>
+                  <TfiCup />
+                </div>
+                <div className={BannerStyled.mini_cart_text}>
+                  <h6>Free Shipping</h6>
+                  <p>Original Product Guarenteed</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
